a8a40346225ecf180302c7035ab1e0144264ef1b
b6320500689 <supitchaya.r@ku.th>
2021-03-29 17:44:21 +0700

    222

--- 6320500689_2.cpp
+++ 6320500689_2.cpp
@@ -1,11 +1,27 @@
 #include <stdio.h>
 int main ()
 {
-	int n;
-	int a[];
+	int n,i,max,j;
+	int a[i];
 	scanf ("%d",&n);
-	scanf ("%d",&a[n]);
-	printf ("%d",a[n]);
+	if (n>= 2 && n<= 1000)
+	{
+		for (i=0; i<n; i++)
+		{
+			scanf ("%d",&a[i]);
+			i++;
+		}
+		max=a[0];
+		for (i=1; i<n; i++)
+		if (a[i]>=max)
+		{
+			max=a[i];
+			j=i;
+		}
+		
+	}
+	
+	printf ("%d",max);
 	return 0;
 	
 }
