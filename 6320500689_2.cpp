5cc2a331da3b5beedbc1f142689bc4c341240509
b6320500689 <supitchaya.r@ku.th>
2021-03-29 19:12:10 +0700

    222222222

--- 6320500689_2.cpp
+++ 6320500689_2.cpp
@@ -1,37 +1,34 @@
 #include <stdio.h>
 int main ()
 {
-	int n,i,max=0,j=0,l;
+	int n,i,max=0,j=0,k;
 	scanf ("%d",&n);
 	int a[n];
+	int l[1001]={0};
 	if (n>= 2 && n<= 1000)
 	{
 		for (i=0; i<n; i++)
 		{
-			scanf ("%d",&a[i]);
+			scanf ("%d",&k);
+			l[k]++;
 		}
-		for (j=0; j<n; j++)
+		for (i=1; i<=1000; i++)
 		{
-			if (j==0)
+			if (max<=l[i])
 			{
-				max=a[j];
+				if (l[i]>0)
+				{
+					a[j]=i;
+					j++;
+				}
+			max=l[i];
 			}
-			if (max<a[j])
-			{
-				max=a[j];
-			}
-		}
-		for (i=0; i<n; i++)
-		{
-			if (max==a[i])
-			{
-				l=i+1;
-				break;
-			}
-		}
+			
+		}	
+		for (i=0; i<j; i++)
+		printf ("%d",a[i]);
 	}
-	for (i=0; i<j; i++)
-		printf ("%d %d",l,max);
+
 	return 0;
 	
 }
