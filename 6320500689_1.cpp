e6950640d981baccc7b3bbcd5e1fc3ca907806ae
b6320500689 <supitchaya.r@ku.th>
2021-03-29 19:12:03 +0700

    1111111

--- 6320500689_1.cpp
+++ 6320500689_1.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-	int n,c,k,a,b=0,y;
+	int n,c,k,a,b=0,y=1,x;
 	int p[32750]={0};
 	scanf ("%d %d",&n,&c);
 	while (a!=0)
@@ -17,6 +17,19 @@ int main ()
 		b=0;
 		a=n;
 		
+		while (a!=0)
+		{
+			p[b]=a%10;
+			a=a/10;
+			if (c==p[b])
+			{
+				x=n;
+				y=0;
+				break;
+			}
+			b++;
+		}
 	}
-	printf ("%d",k);
+	printf ("%d",x);
+	return 0;
 }
