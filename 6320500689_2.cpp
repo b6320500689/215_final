93712b42891c1446ee8246d5694d6136986fca99
b6320500689 <supitchaya.r@ku.th>
2021-03-29 17:56:40 +0700

    2222

--- 6320500689_2.cpp
+++ 6320500689_2.cpp
@@ -2,26 +2,28 @@
 int main ()
 {
 	int n,i,max,j;
-	int a[i];
 	scanf ("%d",&n);
+	int a[n];
 	if (n>= 2 && n<= 1000)
 	{
 		for (i=0; i<n; i++)
 		{
 			scanf ("%d",&a[i]);
-			i++;
 		}
 		max=a[0];
 		for (i=1; i<n; i++)
-		if (a[i]>=max)
 		{
-			max=a[i];
-			j=i;
+			if (a[i]>=max)
+			{
+				max=a[i];
+				j=i;
+			}
 		}
-		
 	}
-	
-	printf ("%d",max);
+	if (max > a[1])
+		printf ("%d",j+1);
+	else
+		printf ("%d %d",j+1,max);
 	return 0;
 	
 }
